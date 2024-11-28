6f4d96c1e258a09b6c0f49bbbef4056f00d40be9
Greeshma594 <greeshmagokuldas369@gmail.com>
2024-11-28 13:57:09 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -17,5 +17,5 @@ echo \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
 newgrp docker
